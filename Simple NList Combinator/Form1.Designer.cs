4227fb11300bcb939a20b76256c928c8afb15cc7
Kaung Myat Thet <35295853+Monojue@users.noreply.github.com>
2021-06-07 23:21:27 +0630

    first

--- Simple NList Combinator/Form1.Designer.cs
+++ Simple NList Combinator/Form1.Designer.cs
@@ -23,13 +23,116 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.pbar = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblCurrent = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(419, 7);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // pbar
+            // 
+            this.pbar.Location = new System.Drawing.Point(12, 88);
+            this.pbar.Name = "pbar";
+            this.pbar.Size = new System.Drawing.Size(482, 23);
+            this.pbar.TabIndex = 1;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(67, 40);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(11, 12);
+            this.lblTotal.TabIndex = 2;
+            this.lblTotal.Text = "0";
+            // 
+            // lblCurrent
+            // 
+            this.lblCurrent.AutoSize = true;
+            this.lblCurrent.Location = new System.Drawing.Point(67, 12);
+            this.lblCurrent.Name = "lblCurrent";
+            this.lblCurrent.Size = new System.Drawing.Size(11, 12);
+            this.lblCurrent.TabIndex = 3;
+            this.lblCurrent.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Current";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Total";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(419, 40);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(506, 123);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCurrent);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.pbar);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.ProgressBar pbar;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblCurrent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
 
--- Simple NList Combinator/Form1.cs
+++ Simple NList Combinator/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,86 @@ namespace Simple_NList_Combinator {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+        }
+
+        string[] nlist = { "a" , "b", "c", "d", "e", "d", "e"};
+        int n = 8;
+        double currentProgress = 0;
+
+        private void btnGenerate_Click(object sender, EventArgs e) {
+            lblTotal.Text = Math.Pow(nlist.Count(), n).ToString();
+            backgroundWorker.RunWorkerAsync(2000);
+        }
+
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e) {
+            //create file
+            //StreamWriter file = new StreamWriter(@"C:\設計書\lines.txt");
+            //create list with given digit n
+            List<int> digit = new List<int>(n);
+            for (int i = 0; i < n; i++) {
+                digit.Add(0);
+            }
+
+            // calculate possible combination
+            double posCOM = Math.Pow(nlist.Count(), n);
+            
+            for (double line = 0; line < posCOM; line++) {
+                if (!backgroundWorker.CancellationPending) {
+                    currentProgress = line + 1;
+                    backgroundWorker.ReportProgress((int)((currentProgress * 100) / posCOM));
+                
+                    string tempLine = "";
+                    //write combination
+                    for (int wpointer = 0; wpointer < n; wpointer++) {
+                        tempLine += nlist[digit[wpointer]];
+                    }
+                   
+                    //file.WriteLineAsync(tempLine);
+                    WriteCharacters(tempLine);
+
+                        //check digit form the latest
+                        for (int cpointer = n - 1; cpointer >= 0; cpointer--) {
+
+                            //if same to no. of nlist count
+                            if (digit[cpointer] == nlist.Count() - 1) {
+                                // reset digit at cpointer to 0
+                                digit[cpointer] = 0;
+                            } else {
+                                //increase 1 to [latest room] -1
+                                digit[cpointer]++;
+                                break;
+                            }
+                        }
+                }
+            }
+            //file.Close();
+        }
+        
+        static string file = @"C:\Export\1.txt";
+        
+        static async void WriteCharacters(string word) {
+            using (StreamWriter writer = File.Exists(file) ? File.AppendText(file) : File.CreateText(file)) {
+                await writer.WriteLineAsync(word);
+            }
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+            pbar.Value = 100;
+            pbar.Update();
+            MessageBox.Show("Done!");
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e) {
+            lblCurrent.Text = currentProgress.ToString();
+            pbar.Value = e.ProgressPercentage;
+            pbar.Update();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e) {
+            
+            if (backgroundWorker.IsBusy)
+                backgroundWorker.CancelAsync();
         }
     }
 }
